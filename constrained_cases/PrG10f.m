5b68ba72327c589a94a59e8bfd47e55df1c9ccbf
yddhebar <yddhebar@gmail.com>
2018-01-18 13:56:53 -0500

    Add files via upload

---
+++ constrained_cases/PrG10f.m
@@ -0,0 +1,3 @@
+function y = PrG10f(x)
+% Matlab Code by A. Hedar (Nov. 23, 2005).
+y = x(1)+x(2)+x(3);
---
+++ constrained_cases/PrG4f.m
@@ -0,0 +1,3 @@
+function y = PrG4f(x)
+% Matlab Code by A. Hedar (Nov. 23, 2005).
+y = 5.3578547*x(3)^2+0.8356891*x(1)*x(5)+37.293239*x(1)-40792.141;
